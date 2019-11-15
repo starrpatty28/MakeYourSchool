0908855ee0db487ae0c49afd9a78f38fd9ee3453
starrpatty28 <starrpatty28@gmail.com>
2019-11-15 14:49:27 -0800

    Fixed google strategy to accepts http and https

--- server/config/prod.js
+++ server/config/prod.js
@@ -6,7 +6,4 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY 
 };
 
-//heroku config:set GOOGLE_CLIENT_ID=178074774606-pooitk1n18s6jpr6bmhjk0461075ss9r.apps.googleusercontent.com
-//heroku config:set GOOGLE_CLIENT_SECRET=sXPaiE23hvu29cO2HfmoGEJb
-//heroku config:set MONGO_URI=mongodb+srv://AliciaLivingston:starrpatty@cluster0-1028p.gcp.mongodb.net/test?retryWrites=true&w=majority
-//heroku config:set COOKIE_KEY=joesmith 
+
