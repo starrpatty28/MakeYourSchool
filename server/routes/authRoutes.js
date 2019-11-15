d25cf798b93829c2fc96d772fce79e5adf7a77cb
starrpatty28 <starrpatty28@gmail.com>
2019-11-15 13:05:56 -0800

    Got my O-Auth fully functining, a user can log-in, authiticate, logout, and the user will be added t the database with a cookie

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+
+// When I pass this function (route handler) it should take me to the auth flow
+
+module.exports = (app) => {
+  app.get(
+    '/auth/google', 
+    passport.authenticate('google', { 
+      scope: ['profile', 'email']
+    })
+  );
+
+  // exchanging the code for the actual user profile
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
+};
