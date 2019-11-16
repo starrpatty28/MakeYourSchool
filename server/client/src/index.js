1b6f64d5b81db5855a881aacd7d097476761ae70
starrpatty28 <starrpatty28@gmail.com>
2019-11-16 15:08:01 -0800

    Import the Provider Tag and some helpers from redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 
 import App from './components/App';
 
