2c892222c67cf2023866c986e7f555172e04f9bf
starrpatty28 <starrpatty28@gmail.com>
2019-11-16 15:10:49 -0800

    stand in to get redux started

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-
 import App from './components/App';
+
+const store = createStore(() => [], )
 
 ReactDOM.render(<App />, document.querySelector('#root')); 
