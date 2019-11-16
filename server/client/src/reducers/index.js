284dbec1528c76af1840f2be9006b2fe184d52ba
starrpatty28 <starrpatty28@gmail.com>
2019-11-16 15:51:10 -0800

    import the reducers and pass the reducrs to the createstore component

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,2 +1,7 @@
 import { combineReducers } from 'redux';
-import authReducer from './authReducer';
+import authReducer from './authReducer'; 
+
+export default combineReducers({
+  auth: authReducer
+})
+
