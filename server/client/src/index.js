6aef52cbbac295355d34e6a8d35029cbee265648
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 12:47:06 -0800

    pass in store as a prop and pulled it from redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,5 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root')); 
+const store = createStore(() => [], {}, applyMiddleware());
 
+ReactDOM.render(
+ <Provider store={store}><App /></Provider>, 
+  document.querySelector('#root')); 
+
+//pass in store as a prop
