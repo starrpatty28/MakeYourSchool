4495259347d818cedb7da7f39d7f1df69e72d917
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 13:04:58 -0800

    create a combineReducers object and pass it a key

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,2 +1,8 @@
 import { combineReducers } from 'redux';
-import authReducer from './authReducer';
+import authReducer from './authReducer'; 
+
+export default combineReducers({
+  auth: authReducer
+}); 
+
+//create a combineReducers object and pass it keys
