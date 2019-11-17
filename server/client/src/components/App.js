b1d36d77ddc6720b4a91a991f27169fe90270e21
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 13:30:04 -0800

    Create come dummy components to make sure they work because it did not work before, time for the re-up

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+//Dummy components
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2> 
+const SurveyNew = () => <h2>SurveyNew</h2> 
+const Landing = () => <h2>Landing</h2> 
+
+
+
 //functional component test
 const App = () => {
   return (
     <div>
-      Yasssss Girl!!!
+      
     </div>
   )
 }
