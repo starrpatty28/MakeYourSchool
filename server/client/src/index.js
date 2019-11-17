d3833bca7291bd0b516a2296b7bed8ed1f6d143d
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 15:36:57 -0800

    tweaked the code in index.js actions, and fixed my code inside of scr index.js

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,13 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxthunk from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
-import thunk from 'redux-thunk';
 
-const store = createStore(reducers, {}, applyMiddleware(redux-thunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
  <Provider store={store}><App /></Provider>, 
