701d7f43380f80cea47c21eea27e5f553323a202
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 10:50:11 -0800

    Redoing React

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,15 +1,12 @@
-import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk'
-;
-import App from './components/App';
-import reducers from './reducers';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
- <Provider store={store}><App></App></Provider>,
- document.querySelector('#root')); 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
