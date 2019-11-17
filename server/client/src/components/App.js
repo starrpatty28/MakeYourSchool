2700d500c1507c23c3c0026738360ad445c3b690
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 15:18:26 -0800

    reate an action creator(initiate change inside our redux application)

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -8,20 +8,21 @@ const Landing = () => <h2>Landing</h2>
 
 
 
-//functional component test
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
-          </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+            <div>
+              <Header />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/surveys" component={Dashboard} />
+              <Route path="/surveys/new" component={SurveyNew} />
+            </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App; 
