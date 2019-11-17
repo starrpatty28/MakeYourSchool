e225b16c57b54893937393c04c76f3c1de779277
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 13:00:17 -0800

    Created my first authReducer function inside my new reducer file

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
 
 const store = createStore(() => [], {}, applyMiddleware());
