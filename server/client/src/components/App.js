91924d6cea92152e6807931d7ab090d586f727f6
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 13:47:30 -0800

    Add the header at the top of the routes to make sure the header will appear at the top of the page

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -15,6 +15,7 @@ const App = () => {
     <div>
       <BrowserRouter>
           <div>
+            <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
             <Route path="/surveys/new" component={SurveyNew} />
