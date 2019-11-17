fe9380dd7cd7e00b68deb7d0f77336f7b7781c54
starrpatty28 <starrpatty28@gmail.com>
2019-11-17 12:11:17 -0800

    refactoring my code

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Yasssss girl, you are Connected!!</h2>
+        <a href="/auth/google">Sign In With Google</a>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
