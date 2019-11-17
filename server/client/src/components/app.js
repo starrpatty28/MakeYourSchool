a3c7449b5eff50203e150536e9520eebeedd3d24
starrpatty28 <starrpatty28@gmail.com>
2019-11-16 16:09:27 -0800

    Created a route path for surveys which is the Dashboard, also made the landing page 'exact{true] path to make sure it has to be that exact path, if not we will have all pages on one page

--- server/client/src/components/app.js
+++ server/client/src/components/app.js
@@ -11,7 +11,8 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" component={Landing} />
+          <Route  exact={true} path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
         </div>
       </BrowserRouter>
     </div>
