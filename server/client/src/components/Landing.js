a92779c68e008e65bc10173527d52d201197995c
starrpatty28 <starrpatty28@gmail.com>
2019-11-18 16:55:39 -0800

    Created a landing page with boiler plate code like the other pages and a quick H1 and feedback message

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>
+        MakeYourSchool
+      </h1>
+      Collect feedback from your students to make a better place to learn!
+    </div>
+
+  )
+
+}
