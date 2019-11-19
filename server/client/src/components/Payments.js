2d943bb10dd024b215be8b6bca8b35be3fd62d6d
starrpatty28 <starrpatty28@gmail.com>
2019-11-18 19:12:38 -0800

    Tweaked the code to the get the Stripe API key checkout to work

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -3,13 +3,16 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render() {
-        
+
     return (
       <StripeCheckout 
         amount={500}
         token={token => console.log(token)}
-        stripeKey={ProcessingInstruction.env.REACT_APP_STRIPE_KEY}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
       />
     )
   }
 }
+
+export default Payments;
+
