9cd702da2b5c50f638726f91733a6af858960631
starrpatty28 <starrpatty28@gmail.com>
2019-11-18 16:58:43 -0800

    Add styling to the landing page to center my text and add code to export the Landing page

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <div>
+    <div style={{ textAlign: 'center'}}>
       <h1>
         MakeYourSchool
       </h1>
       Collect feedback from your students to make a better place to learn!
     </div>
-
   )
+
+  export default Landing;
+
+
 
 }
