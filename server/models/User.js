15aac5b6c417449c87d949d6e6382a7b76544c60
starrpatty28 <starrpatty28@gmail.com>
2019-11-19 09:30:04 -0800

    Gave my user model a credit property

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// User schema
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
