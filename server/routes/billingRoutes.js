60038f66edebeca5a5a53169bb45b997cbab4dd6
starrpatty28 <starrpatty28@gmail.com>
2019-11-19 10:33:06 -0800

    Check if the user is signed in

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,12 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if(!req.user) {
+      return res.status(401).send({ error: 'You must log in!' })
+    }
+  })
+
+  app.post('/api/stripe', async (req, res) => {
     const charge = await stripe.charges.create ({
      amount: 300,
      currency: 'usd',
