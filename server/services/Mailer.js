66788115ec41e7f656dc23f7a863d2f9d31aff7e
starrpatty28 <starrpatty28@gmail.com>
2019-11-20 15:24:59 -0800

    Make sure our credits are being updated for the user, create an error handler just incase the email don’t go through, send a error message of 422 Unprocessable Entity you sent the wrong data

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -48,7 +48,7 @@ class Mailer extends helper.Mail {
       body: this.toJSON()
     })
 
-    const response = this.sgAPI.API(request);
+    const response = await this.sgAPI.API(request);
     return response;
   }
 } 
