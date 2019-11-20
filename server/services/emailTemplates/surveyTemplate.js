6d7fc09312a8879d1a423bb46eb91cb4053ce23a
starrpatty28 <starrpatty28@gmail.com>
2019-11-20 14:58:22 -0800

    Added some html for now, will design later

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,4 +1,18 @@
 module.exports = (survey) => {
-  return '<div>' + survey.body + '</div>';
+  return ` 
+    <html>
+      <body>
+        <div style="text-align: center">
+          <h3> We'd like to know how to make your school a better place to learn!</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+          <a href="http://localhost:3000">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </body>
+    </html> 
+  `;
 };
  
