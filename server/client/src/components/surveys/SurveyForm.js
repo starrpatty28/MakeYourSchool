a6c176651389e2e4a2d26a7cb635e4806ddbfcd8
starrpatty28 <starrpatty28@gmail.com>
2019-11-21 13:20:17 -0800

    Refactored my code per my mentor

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -3,37 +3,19 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+  { label: 'Survey Ttile', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+  
+]
+
 class SurveyForm extends Component {
 
   renderFields() {
-    return (
-      <div>
-        <Field
-          label="Survey Title" 
-          type="text" 
-          name="title" 
-          component={SurveyField} 
-        />
-        <Field
-          label="Subject Line" 
-          type="text" 
-          name="subject" 
-          component={SurveyField} 
-        />
-        <Field
-          label="Email Body" 
-          type="text" 
-          name="body" 
-          component={SurveyField} 
-        />
-        <Field
-          label="Recipient List" 
-          type="text" 
-          name="emails" 
-          component={SurveyField} 
-        />
-      </div>
-    )
+   
+    
   }
 
   render () {
