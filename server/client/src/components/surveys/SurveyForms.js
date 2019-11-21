a129628e3486ea83bfb3b167505b52ed941baef0
starrpatty28 <starrpatty28@gmail.com>
2019-11-20 18:42:23 -0800

    Created a SurveyForm for to import in SurveyNew, copied the boiler plate code from SurveyNew and exported the same way, thanks React

---
+++ server/client/src/components/surveys/SurveyForms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyForm extends Component {
+  render () {
+    return (
+      <div>
+        Hello World, Survey Form!!
+      </div>
+    )
+  }
+}
+
+export default SurveyForm;
