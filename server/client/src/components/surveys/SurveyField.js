f30bb28bc8e318fbd6361bf055e59c029e126b4a
starrpatty28 <starrpatty28@gmail.com>
2019-11-20 20:15:42 -0800

    Taking a bunch of event handlers from redux form, wired them to the input element with a “Spread Attribute”, that input element is being rendered by the field

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+//SurveyField will contain logic to render a single label and text input
+import React from 'react';
+
+export default ({ input }) => {
+
+  return (
+    <div>
+      <input {...input} />
+    </div>
+  )
+} 
