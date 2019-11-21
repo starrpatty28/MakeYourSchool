032184373b56b4c2bca9f01969c5ce695b48b53a
starrpatty28 <starrpatty28@gmail.com>
2019-11-20 19:01:35 -0800

    import the redux-form helper

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,5 +1,6 @@
 // SurveyForm shows a form for a user to add input
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class SurveyForm extends Component {
   render () {
