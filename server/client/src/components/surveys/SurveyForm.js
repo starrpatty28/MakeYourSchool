e6423f813b802395b3a2270e9df28261b2e9abf1
starrpatty28 <starrpatty28@gmail.com>
2019-11-21 18:57:53 -0800

    Had a bug after my email validation and some code was wrong, it works now, I used a Regex email validator

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: 'Survey Ttile', name: 'title' },
@@ -41,11 +42,15 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.email || '');
+
    _.each(FIELDS, ({ name }) => {
      if(!values[name]) {
        errors[name] = 'You must provide a value'
      }
    });
+
 
   return errors;
 }
