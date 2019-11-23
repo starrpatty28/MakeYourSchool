321cf49f20bca7ec9c2b5946a94cc0897a1cda86
starrpatty28 <starrpatty28@gmail.com>
2019-11-23 14:54:37 -0800

    Wrote my first real querie w/the help of some awesome Mentorsgit add .

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -13,17 +13,38 @@ const Survey = mongoose.model('surveys');
 
 // Request Handler
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thank You For Making Your School A Better Place');
   })
 
   // testing my webhooks 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, (event) => {
-      const pathname = new URL(event.url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      console.log(p.test(pathname));
-    });
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+        .map(({ email, url}) => { 
+          const match = p.test(new URL(url).pathname);
+          if (match) {
+            return { email, surveyId: match.surveyId, choice: match.choice };
+          }
+        })
+        .compact()
+        .uniqBy('email', 'surveyId')
+        .each(({ surveyId, email, choice}) => {
+          Survey.updateOne({
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          }, { 
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true},
+            lastResponded: new Date()
+          }
+          ).exec()  
+        })
+        .value();
+    res.send({});
   });
 
 
