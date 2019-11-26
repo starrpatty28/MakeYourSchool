abc1bafb512232ec7399f21006d2b7fbca67269a
starrpatty28 <starrpatty28@gmail.com>
2019-11-26 13:14:57 -0800

    Tweaked typo

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -7,7 +7,7 @@ const Landing = () => {
       <h1>
         Make Your School
       </h1>
-      <h5>Collect feedback from your students to make a better place to learn</h5>
+      <h5>Collect feedback from your students to create a safe and better place to learn</h5>
       <img src={Highschool} /> 
     </div>
   )
